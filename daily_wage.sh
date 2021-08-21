3aec3b741ff63ff04f6a9e5e7ceeadc541c3ff75
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-08-21 21:47:09 +0530

    Program to calculate daily wages of employee

---
+++ daily_wage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+Full_Time=1
+Part_Time=2
+Wage_per_hr=20
+Working_days=22
+salary=0
+
+for (( days=1; days<=$Working_days; days++ ))
+do
+	Empl_check=$((RANDOM%3));
+	if [ $Empl_check -eq $Full_Time ]
+	then
+		emphrs=8
+	elif [ $Empl_check -eq $Part_Time ]
+	then
+		emphrs=4
+	else
+		emphrs=0
+	fi
+
+	Emp_Wages=$(( $emphrs * $Wage_per_hr ))
+	salary=$(($salary + $Emp_Wages))
+done
+echo "Employees Salary: $salary"
