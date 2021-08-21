c2c69e3ad7b09f76b7aaceced75ce33f58611968
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-08-21 08:41:56 +0530

    Employee_attendance

---
+++ check_attendance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+randNo=$((RANDOM%3))
+attendance=0
+for (( i=1; i<=20; i++ ))
+do
+	read n
+	if [ $randNo -eq $n ]
+	then
+		echo "Employee is present"
+		$((attendance+=1))
+	else
+		echo "Employee is absent"
+	fi
+done
+echo "$attendance"
