3720b094a04cce6c0f0eccaf6b964bcb076df6e7
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-08-22 13:16:04 +0530

    Program to calculate wages of employees till total days or hrs is reached

---
+++ wage_by_days_or_hrs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+Full_Time=1
+Part_Time=2
+Wage_per_hr=20
+Working_days=20
+salary=0
+Total_hrs=0
+
+for (( days=emphrs=1; days<=$Working_days && emphrs<=100; days++,emphrs++ ))
+do
+   Empl_check=$((RANDOM%3));
+   if [ $Empl_check -eq $Full_Time ]
+   then
+      emphrs=8
+   elif [ $Empl_check -eq $Part_Time ]
+   then
+      emphrs=4
+   else
+      emphrs=0
+   fi
+
+	Total_hrs=$(( Total_hrs + emphrs ))	
+   Emp_Wages=$(( $emphrs * $Wage_per_hr ))
+   salary=$(($salary + $Emp_Wages))
+done
+echo "Employees Salary: $salary"
+echo "$Total_hrs"
